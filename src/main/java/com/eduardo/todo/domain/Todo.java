e17779c86adb797b8ea441fb925cf32bf42bee41
NoteEdu <eduardo.tsilva@hotmail.com>
2021-04-08 18:07:09 -0300

    5. Iniciando classe de domínio

---
+++ src/main/java/com/eduardo/todo/domain/Todo.java
@@ -0,0 +1,95 @@
+package com.eduardo.todo.domain;
+
+import java.time.LocalDate;
+
+public class Todo {
+	
+	private Integer id;
+	private String titulo;
+	private String descricao;
+	private LocalDate dataParaFinalizar;
+	private Boolean finalizado = false;
+	
+	public Todo() {
+		super();
+	}
+
+	public Todo(Integer id, String titulo, String descricao, LocalDate dataParaFinalizar, Boolean finalizado) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.descricao = descricao;
+		this.dataParaFinalizar = dataParaFinalizar;
+		this.finalizado = finalizado;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDate getDataParaFinalizar() {
+		return dataParaFinalizar;
+	}
+
+	public void setDataParaFinalizar(LocalDate dataParaFinalizar) {
+		this.dataParaFinalizar = dataParaFinalizar;
+	}
+
+	public Boolean getFinalizado() {
+		return finalizado;
+	}
+
+	public void setFinalizado(Boolean finalizado) {
+		this.finalizado = finalizado;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Todo other = (Todo) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+
+}
+
