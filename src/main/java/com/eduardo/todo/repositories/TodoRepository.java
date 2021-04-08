18056aa71e0741385bab0dcb3541ac30d5172ce4
NoteEdu <eduardo.tsilva@hotmail.com>
2021-04-08 18:28:28 -0300

    7. Configurando Banco de Dados

---
+++ src/main/java/com/eduardo/todo/repositories/TodoRepository.java
@@ -0,0 +1,11 @@
+package com.eduardo.todo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eduardo.todo.domain.Todo;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Integer> {
+
+}
